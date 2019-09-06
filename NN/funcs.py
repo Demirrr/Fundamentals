f58043028da5044bcdf1be057eac12b1470842db
Demir <caglardemir8@gmail.com>
2019-09-06 11:18:06 +0200

    more and more

---
+++ NN/funcs.py
@@ -0,0 +1,152 @@
+import numpy as np
+import random
+
+def softmax(x):
+    """Compute the softmax function for each row of the input x.
+
+    It is crucial that this function is optimized for speed because
+    it will be used frequently in later code. You might find numpy
+    functions np.exp, np.sum, np.reshape, np.max, and numpy
+    broadcasting useful for this task.
+
+    Numpy broadcasting documentation:
+    http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
+
+    You should also make sure that your code works for a single
+    N-dimensional vector (treat the vector as a single row) and
+    for M x N matrices. This may be useful for testing later. Also,
+    make sure that the dimensions of the output match the input.
+
+    You must implement the optimization in problem 1(a) of the
+    written assignment!
+
+    Arguments:
+    x -- A N dimensional vector or M x N dimensional numpy matrix.
+
+    Return:
+    x -- You are allowed to modify x in-place
+    """
+    orig_shape = x.shape
+
+    if len(x.shape) > 1:
+        # Matrix
+        ### YOUR CODE HERE
+        exp_minmax = lambda x: np.exp(x - np.max(x))
+        denom = lambda x: 1.0 / np.sum(x)
+        x = np.apply_along_axis(exp_minmax, 1, x)
+        denominator = np.apply_along_axis(denom, 1, x)
+
+        if len(denominator.shape) == 1:
+            denominator = denominator.reshape((denominator.shape[0], 1))
+
+        x = x * denominator
+        ### END YOUR CODE
+    else:
+        # Vector
+        ### YOUR CODE HERE
+        x_max = np.max(x)
+        x = x - x_max
+        numerator = np.exp(x)
+        denominator = 1.0 / np.sum(numerator)
+        x = numerator.dot(denominator)
+        ### END YOUR CODE
+    return x
+
+def sigmoid(x):
+    """
+    Compute the sigmoid function for the input here.
+
+    Arguments:
+    x -- A scalar or numpy array.
+
+    Return:
+    s -- sigmoid(x)
+    """
+
+    ### YOUR CODE HERE
+    s = 1.0 / (1 + np.exp(-x))
+    ### END YOUR CODE
+
+    return s
+
+
+def sigmoid_grad(s):
+    """
+    Compute the gradient for the sigmoid function here. Note that
+    for this implementation, the input s should be the sigmoid
+    function value of your original input x.
+
+    Arguments:
+    s -- A scalar or numpy array.
+
+    Return:
+    ds -- Your computed gradient.
+    """
+
+    ### YOUR CODE HERE
+    ds = s * (1 - s)
+    ### END YOUR CODE
+
+    return ds
+
+#!/usr/bin/env python
+
+import numpy as np
+import random
+
+
+# First implement a gradient checker by filling in the following functions
+def gradcheck_naive(f, x):
+    """ Gradient check for a function f.
+
+    Arguments:
+    f -- a function that takes a single argument and outputs the
+         cost and its gradients
+    x -- the point (numpy array) to check the gradient at
+    """
+
+    rndstate = random.getstate()
+    random.setstate(rndstate)
+    fx, grad = f(x) # Evaluate function value at original point
+    h = 1e-4        # Do not change this!
+
+    # Iterate over all indexes in x
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        ix = it.multi_index
+
+        # Try modifying x[ix] with h defined above to compute
+        # numerical gradients. Make sure you call random.setstate(rndstate)
+        # before calling f(x) each time. This will make it possible
+        # to test cost functions with built in randomness later.
+
+        ### YOUR CODE HERE:
+        x[ix] += h
+
+        random.setstate(rndstate)
+        new_f1 = f(x)[0]
+
+        x[ix] -= 2*h
+
+        random.setstate(rndstate)
+        new_f2 = f(x)[0]
+
+        x[ix] += h
+
+        numgrad = (new_f1 - new_f2) / (2 * h)
+        ### END YOUR CODE
+
+        # Compare gradients
+        reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
+        if reldiff > 1e-5:
+            print ("Gradient check failed.")
+            print ("First gradient error found at index %s" % str(ix))
+            print ("Your gradient: %f \t Numerical gradient: %f" % (
+                grad[ix], numgrad))
+            return
+
+        it.iternext() # Step to next dimension
+
+    print ("Gradient check passed!")
+
+
